79d3c1e31cfd1b746192040059d5bce488890d2e
Stefan Bielak <117030658+StefanBielak@users.noreply.github.com>
2023-11-27 20:18:36 +0100

    .json-update_for_git

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store';
-import App from './app';
+import store from './components/store';
+import App from './components/App';
 
 ReactDOM.render(
   <Provider store={store}>
