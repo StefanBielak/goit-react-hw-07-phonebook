90e3d6f378ea38cb3a40d714b0ee372fbe7c1e4a
Stefan Bielak <117030658+StefanBielak@users.noreply.github.com>
2023-11-27 20:14:34 +0100

    app/index-update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact, deleteContact } from './actions';
+import { setFilter } from './actions';
 import ContactForm from '#components/ContactForm/ContactForm';
 import ContactList from '#components/ContactList/ContactList';
 import Filter from '#components/Filter/Filter';
-import styles from "#components/App.module.css"
+import styles from "#components/App.module.css";
 
-export const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    const savedContacts = localStorage.getItem('contacts');
-    return savedContacts ? JSON.parse(savedContacts) : [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ];
-  });
-
-  const [filter, setFilter] = useState('');
+const App = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector((state) => state.contacts);
+  const filter = useSelector((state) => state.filter);
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
-  const addContact = ({ name, number }) => {
-    const newContact = { id: nanoid(), name, number };
-    if (!contacts.some((contact) => contact.name.toLowerCase() === name.toLowerCase())) {
-      setContacts([...contacts, newContact]);
-    } else {
-      alert('Contact with this name already exists!');
-    }
-  };
-
-  const deleteContact = (id) => {
-    setContacts(contacts.filter((contact) => contact.id !== id));
-  };
-
   const handleFilterChange = (event) => {
-    setFilter(event.target.value);
+    dispatch(setFilter(event.target.value));
   };
 
   const filteredContacts = contacts.filter((contact) =>
@@ -46,10 +27,12 @@ export const App = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Phonebook</h1>
-      <ContactForm addContact={addContact} />
+      <ContactForm addContact={(contact) => dispatch(addContact(contact))} />
       <h2 className={styles.title}>Contacts</h2>
       <Filter value={filter} onChange={handleFilterChange} />
-      <ContactList contacts={filteredContacts} onDelete={deleteContact} />
+      <ContactList contacts={filteredContacts} onDelete={(id) => dispatch(deleteContact(id))} />
     </div>
   );
 };
+
+export default App;
